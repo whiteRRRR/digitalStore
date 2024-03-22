82de945158ea0ff51b6319082b6897eafdd3a39e
whiteRR <whiterr2134@gmail.com>
2024-03-22 19:39:30 +0500

    Created Settings for applications

---
+++ services/Auth/core/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseSettings, BaseModel
+from os import getenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class DataBaseSettings(BaseModel):
+    database_url: str = getenv("DATABASE_URL")
+
+
+class JWTSettings(BaseModel):
+    private_key: Path = BASE_DIR / "certs" / "private.pem"
+    public_key: Path = BASE_DIR / "certs" / "public.pem"
+    algorithm: str = "RS256"
+
+
+class Settings(BaseSettings):
+    jwt_settings: JWTSettings = JWTSettings()
+    database: DataBaseSettings = DataBaseSettings()
+
+
+settings = Settings()

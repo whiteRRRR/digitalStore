e33032411c80ed425ac37dc8d3e48ad25e0ff4b7
whiteRR <whiterr2134@gmail.com>
2024-03-26 13:03:38 +0500

    Created baseService

---
+++ apps/Auth/services/baseService.py
@@ -0,0 +1,19 @@
+from repositories import baseRepository
+from repositories.baseRepository import BaseRepository
+
+
+class BaseService:
+    def __init__(self, repository):
+        self.repository = repository
+
+    async def get_by_id(self, id: int):
+        return await self.repository.read_by_id(id)
+
+    async def add(self, schema):
+        return await self.repository.create(schema)
+
+    async def update_by_id(self, id: int, schema):
+        return await self.repository.update_by_id(id, schema)
+
+    async def delete_by_id(self, id: int):
+        return await self.repository.delete_by_id(id)

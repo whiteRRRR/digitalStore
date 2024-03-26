25f4423dd0148580f54104813bddb8feee92c0b5
whiteRR <whiterr2134@gmail.com>
2024-03-26 12:50:00 +0500

    Fix bug

--- apps/Auth/alembic/env.py
+++ apps/Auth/alembic/env.py
@@ -19,14 +19,19 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from core.database import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv('D:\\Projects\\Python\\FAST API\\digitalStore\\.env')
 
-db_url = "postgresql+asyncpg://whiteRR:whiteRR2134@localhost:5432/authUserDB"
+db_url = getenv('DATABASE_URL')
 config.set_main_option("sqlalchemy.url", db_url)
 
 

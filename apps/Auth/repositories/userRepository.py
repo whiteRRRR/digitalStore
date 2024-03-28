e93e037cc556a3b5f3e5a7314f170947be666924
whiteRR <whiterr2134@gmail.com>
2024-03-28 13:21:14 +0500

    added read_user_by_username function

--- apps/Auth/repositories/userRepository.py
+++ apps/Auth/repositories/userRepository.py
@@ -1,8 +1,20 @@
 from .baseRepository import BaseRepository
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from models.userDB import User
 
 
 class UserRepository(BaseRepository):
     def __init__(self, session: AsyncSession):
         super().__init__(User, session)
+
+    async def read_user_by_username(self, username: str):
+        stmt = await self.session.execute(
+            select(
+                self.model
+            ).where(
+                self.model.username == username
+            )
+        )
+        user = stmt.scalar()
+        return user

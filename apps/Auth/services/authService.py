4881afaa5c94163869d50bc24e26a79d032ca7ba
whiteRR <whiterr2134@gmail.com>
2024-03-28 13:23:17 +0500

    added sign-in service function

--- apps/Auth/services/authService.py
+++ apps/Auth/services/authService.py
@@ -1,7 +1,7 @@
 from .baseService import BaseService
 from repositories.userRepository import UserRepository
-from repositories.authRepository import AuthorizationRepository
-from schemes.authScheme import SignUp
+from repositories.authRepository import AuthRepository
+from schemes.authScheme import SignUp, SignIn, UserPayload, UserWithToken
 from core.security import security
 
 
@@ -9,10 +9,11 @@ class AuthService(BaseService):
 
     def __init__(
             self,
+            auth_repository: AuthRepository,
             user_repository: UserRepository,
     ):
         self.user_repository = user_repository
-
+        self.auth_repository = auth_repository
         super().__init__(user_repository)
 
     async def sign_up(self, user_data: SignUp):
@@ -20,4 +21,21 @@ class AuthService(BaseService):
         user_data.hashed_password = hashed_password
         created_user = await self.add(user_data)
         return created_user
+
+    # TODO: ADD Exceptions
+    async def sign_in(self, user_data: SignIn):
+        user_info = await self.user_repository.read_user_by_username(user_data.username)
+        verify_password = await security.password_security.check_password(user_data.password,
+                                                                          user_info.hashed_password)
+        if verify_password:
+            user_payload = UserPayload(username=user_info.username)
+            user_refresh_token = await security.jwt_security.create_refresh_token(user_payload)
+
+            user = UserWithToken(user_id=user_info.id, refresh_token=user_refresh_token)
+            create_user_with_token = await self.auth_repository.create(user)
+            return create_user_with_token
+
+
+
+
 

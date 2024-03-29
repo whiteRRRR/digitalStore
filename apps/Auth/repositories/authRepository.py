123362a0a7838cae0be1d413f7c0dfa5e881c488
whiteRR <whiterr2134@gmail.com>
2024-03-29 13:45:55 +0500

    Added update_refresh_token method to AuthRepository

--- apps/Auth/repositories/authRepository.py
+++ apps/Auth/repositories/authRepository.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy import select
 from .baseRepository import BaseRepository
 from models.userTokenDB import UserToken
 
@@ -8,3 +8,14 @@ class AuthRepository(BaseRepository):
     def __init__(self, session: AsyncSession):
         super().__init__(UserToken, session)
 
+    async def update_refresh_token(self, user_id: int, new_refresh_token: str):
+        stmt = await self.session.execute(
+            select(
+                self.model
+            ).where(
+                self.model.user_id == user_id
+            )
+        )
+
+        stmt.refresh_token = new_refresh_token
+        await self.session.commit()

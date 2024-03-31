0c39d74922381ad4f8eb854ac027d695ccf98859
whiteRR <whiterr2134@gmail.com>
2024-03-31 17:42:43 +0500

    fixed sign_in endpoint

--- apps/Auth/api/v1/endpoints/auth.py
+++ apps/Auth/api/v1/endpoints/auth.py
@@ -1,19 +1,17 @@
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import connect_database
 from repositories.userRepository import UserRepository
 from repositories.authRepository import AuthRepository
 from services.authService import AuthService
-from schemes.authScheme import SignIn, SignUp, UserWithoutPassword
+from schemes.authScheme import SignIn, SignUp
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/sign-up")
 
 
-@router.post("/sign-up", response_model=UserWithoutPassword)
-async def sign_up(user_data: SignUp, session: AsyncSession = Depends(connect_database)) -> UserWithoutPassword:
+@router.post("/sign-up/", status_code=status.HTTP_201_CREATED)
+async def sign_up(user_data: SignUp, session: AsyncSession = Depends(connect_database)) -> str:
     user_repository = UserRepository(session)
     auth_repository = AuthRepository(session)
     auth_service = AuthService(auth_repository, user_repository)
@@ -21,10 +19,10 @@ async def sign_up(user_data: SignUp, session: AsyncSession = Depends(connect_dat
     return await auth_service.sign_up(user_data)
 
 
-@router.post("/sign-in")
-async def sign_in(user_data: SignIn, session: AsyncSession = Depends(connect_database)):
+@router.post("/sign-in/")
+async def sign_in(user_data: SignIn, response: Response, session: AsyncSession = Depends(connect_database)):
     user_repository = UserRepository(session)
     auth_repository = AuthRepository(session)
     auth_service = AuthService(auth_repository, user_repository)
 
-    return await auth_service.sign_in(user_data)
+    return await auth_service.sign_in(user_data, response)

04b39f29e951804abc460d31a6418bf083edb440
whiteRR <whiterr2134@gmail.com>
2024-04-05 11:14:22 +0500

    added user_router

--- apps/Auth/api/v1/routers.py
+++ apps/Auth/api/v1/routers.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from api.v1.endpoints.auth import router as auth_router
+from api.v1.endpoints.user import router as user_router
 
 router = APIRouter()
 router.include_router(auth_router)
+router.include_router(user_router)

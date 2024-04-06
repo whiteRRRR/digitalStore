9c07ae79ab6cf2acb3254c3f71cdc5b076a09a57
whiteRR <whiterr2134@gmail.com>
2024-04-06 22:47:05 +0500

    Added new exception

--- apps/Auth/core/exceptions.py
+++ apps/Auth/core/exceptions.py
@@ -4,3 +4,8 @@ from fastapi import HTTPException, status
 class AuthException(HTTPException):
     def __init__(self, detail: str, headers: dict | None = None) -> None:
         super().__init__(status.HTTP_401_UNAUTHORIZED, detail, headers)
+
+
+class BadRequestException(HTTPException):
+    def __init__(self, detail: str, headers: dict | None = None) -> None:
+        super().__init__(status.HTTP_400_BAD_REQUEST, detail, headers)

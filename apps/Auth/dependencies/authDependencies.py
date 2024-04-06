a661b418d78d324e625a8dd9f73c5f8725fc99b2
whiteRR <whiterr2134@gmail.com>
2024-04-06 22:46:01 +0500

    Fixed bug

--- apps/Auth/dependencies/authDependencies.py
+++ apps/Auth/dependencies/authDependencies.py
@@ -6,14 +6,15 @@ from jwt.exceptions import InvalidSignatureError
 
 async def check_authenticate(request: Request) -> str:
     cookie_data = request.cookies.get("auth")
-
+    if cookie_data is None:
+        raise AuthException("No cookie found")
     try:
         token_info = await security.jwt_security.decode_jwt(cookie_data)
+        print(token_info)
     except InvalidSignatureError:
         raise AuthException("Token invalid")
 
     return token_info["username"]
-
 
 
 

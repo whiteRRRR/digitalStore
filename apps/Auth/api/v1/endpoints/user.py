1d38d086b0d5687893e8e1fa8dd3635abed955ff
whiteRR <whiterr2134@gmail.com>
2024-04-10 16:22:19 +0500

    created several methods for changing the password, deleting the user and resetting the password

--- apps/Auth/api/v1/endpoints/user.py
+++ apps/Auth/api/v1/endpoints/user.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import connect_database
 from repositories.userRepository import UserRepository
 from schemes.userScheme import UserWithoutPassword
 from dependencies.authDependencies import check_authenticate
 from services.userService import UserService
+from schemes.userScheme import UserChangePassword, UserResetPassword
 
 router = APIRouter(prefix="/user", tags=["user"])
 
@@ -14,6 +15,59 @@ async def me(
         session: AsyncSession = Depends(connect_database),
         current_user: str = Depends(check_authenticate),
 ) -> UserWithoutPassword:
+    # Change
     user_repository = UserRepository(session)
     user_service = UserService(user_repository)
+
     return await user_service.user_info(current_user)
+
+
+@router.put("/me/change_password/", status_code=status.HTTP_200_OK)
+async def change_password(
+        passwords: UserChangePassword,
+        session: AsyncSession = Depends(connect_database),
+        current_user: str = Depends(check_authenticate),
+):
+    # Change
+    user_repository = UserRepository(session)
+    user_service = UserService(user_repository)
+
+    return await user_service.change_user_password(current_user, passwords)
+
+
+@router.delete("/me/delete/", status_code=status.HTTP_202_ACCEPTED)
+async def delete_me(
+        response: Response,
+        session: AsyncSession = Depends(connect_database),
+        current_user: str = Depends(check_authenticate),
+):
+    # Change
+    user_repository = UserRepository(session)
+    user_service = UserService(user_repository)
+
+    return await user_service.delete_user(response, current_user)
+
+
+@router.get("/forgot-password")
+async def forgot_password(
+        email: str,
+        session: AsyncSession = Depends(connect_database)
+):
+    # Change
+    user_repository = UserRepository(session)
+    user_service = UserService(user_repository)
+
+    return await user_service.forgot_password(email)
+
+
+@router.put("/resset_password/{secret_key}")
+async def reset_password(
+        secret_key: str,
+        new_password: UserResetPassword,
+        session: AsyncSession = Depends(connect_database)
+):
+    # Change
+    user_repository = UserRepository(session)
+    user_service = UserService(user_repository)
+
+    return await user_service.resset_password(secret_key, new_password)
--- apps/Auth/services/userService.py
+++ apps/Auth/services/userService.py
@@ -1,5 +1,12 @@
+import json
 from .baseService import BaseService
 from repositories.userRepository import UserRepository
+from core.security import security
+from core.config import SERVER_HOST, settings
+from core.exceptions import BadRequestException
+from fastapi import Response, BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from schemes.userScheme import UserChangePassword, UserEmail, UserResetPassword
 
 
 class UserService(BaseService):
@@ -8,6 +15,87 @@ class UserService(BaseService):
         super().__init__(user_repository)
 
     async def user_info(self, username: str):
-        user_data = await self.user_repository.read_user_by_username(username)
-        return user_data
+        user = await self.user_repository.read_user_by_username(username)
+
+        if not user:
+            raise BadRequestException("Bad Request")
+
+        return user
+
+    async def delete_user(self, response: Response, username: str):
+        user = await self.user_repository.read_user_by_username(username)
+
+        if not user:
+            raise BadRequestException("Bad Request")
+
+        await self.user_repository.delete_user_by_username(user.username)
+        response.delete_cookie("auth")
+        return {"message": "User deleted successfully"}
+
+    async def change_user_password(self, username: str, user_passwords: UserChangePassword):
+        user = await self.user_repository.read_user_by_username(username)
+
+        if not user:
+            raise BadRequestException("Bad Request")
+
+        if await security.password_security.check_password(user_passwords.old_password, user.hashed_password):
+            hashed_password = await security.password_security.get_hash_password(user_passwords.new_password)
+            await self.user_repository.update_user_password(user.username, hashed_password)
+            return {"message": "Password changed successfully"}
+
+    async def forgot_password(self, email: str):
+        user = await self.user_repository.read_user_by_email(email)
+
+        if not user:
+            raise BadRequestException("Invalid Email Address")
+
+        fm = FastMail(settings.email.email_conf)
+        email_payload = UserEmail(email=email)
+        secret_key = await security.jwt_security.create_secret_key(email_payload)
+        forgot_url_link = f"{SERVER_HOST}//forgot-password//{secret_key}//"
+        email_body = {
+            "company_name": settings.email.mail_from,
+            "reset_link": forgot_url_link,
+        }
+        email_body_to_js = json.dumps(email_body)
+        message = MessageSchema(
+            subject="Password Reset Request",
+            recipients=[email],
+            template_body=email_body_to_js,
+            subtype=MessageType.html
+        )
+
+        await fm.send_message(message)
+        return {"message": "Email sent successfully"}
+
+    async def resset_password(self, secret_key: str, new_password: UserResetPassword):
+
+        try:
+            check_secret_key = await security.jwt_security.decode_jwt(secret_key)
+            user_email = check_secret_key["email"]
+            user = await self.user_repository.read_user_by_email(user_email)
+            hashed_password = await security.password_security.get_hash_password(new_password.new_password)
+            await self.user_repository.update_user_password(user.username, hashed_password)
+            return {"message": "Password successfully updated"}
+        except BadRequestException:
+            raise BadRequestException("Invalid email")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

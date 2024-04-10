ba8c80a1b7c2e5e8d7696bf9a50f862fab0bdd34
whiteRR <whiterr2134@gmail.com>
2024-04-10 16:19:51 +0500

    added new methods to read user email and update user password

--- apps/Auth/repositories/userRepository.py
+++ apps/Auth/repositories/userRepository.py
@@ -1,6 +1,6 @@
 from .baseRepository import BaseRepository
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from models.userDB import User
 
 
@@ -18,3 +18,38 @@ class UserRepository(BaseRepository):
         )
         user = stmt.scalar()
         return user
+
+    async def read_user_by_email(self, email: str):
+        stmt = await self.session.execute(
+            select(
+                self.model
+            ).where(
+                self.model.email == email
+            )
+        )
+        user = stmt.scalar()
+        return user
+
+    async def update_user_password(self, username: str, new_password: bytes):
+        stmt = await self.session.execute(
+            select(
+                self.model
+            ).where(
+                self.model.username == username
+            )
+        )
+
+        user = stmt.scalar()
+        user.hashed_password = new_password
+        await self.session.commit()
+
+    async def delete_user_by_username(self, username: str):
+        await self.session.execute(
+            delete(
+                self.model
+            ).where(
+                self.model.username == username
+            )
+        )
+
+        await self.session.commit()

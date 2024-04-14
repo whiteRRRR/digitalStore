5f582ecf4d5a36f6b6673de5d98f48bfdb355750
whiteRR <whiterr2134@gmail.com>
2024-04-14 19:05:16 +0500

    Created baseRepository

---
+++ apps/Category/repositories/baseRepository.py
@@ -0,0 +1,57 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, delete
+
+class BaseRepository:
+    def __init__(self, model, session: AsyncSession) -> None:
+        self.model = model
+        self.session = session
+
+    async def create(self, scheme):
+        new_obj = self.model(**scheme.model_dump())
+
+        self.session.add(new_obj)
+        await self.session.commit()
+        await self.session.refresh(new_obj)
+        return new_obj
+    
+    async def read_by_id(self, id: int):
+        stmt = await self.session.execute(
+            select(
+                self.model
+            ).where(
+                self.model.id == id
+            )
+        )
+
+        obj_info = stmt.scalar()
+        return obj_info
+    
+    async def update_by_id(self, id: int, scheme):
+        try:
+            await self.session.execute(
+                update(
+                    self.model
+                ).where(
+                    self.model.id == id
+                ).values(
+                    scheme.model_dump()
+                )
+            )
+            await self.session.commit()
+            return "Success full updated"
+        except Exception:
+            raise Exception("Error")
+
+    async def delete_by_id(self, id: int):
+        try:
+            await self.session.execute(
+                delete(
+                    self.model
+                ).where(
+                    self.model.id == id
+                )
+            )
+            return "Success full deleted item"
+        except Exception:
+            raise Exception("Error")
+        

ebc34e6a13d6447f012e56008d7288637b034226
whiteRR <whiterr2134@gmail.com>
2024-04-15 16:23:21 +0500

    change return type

--- apps/Auth/core/database.py
+++ apps/Auth/core/database.py
@@ -1,3 +1,4 @@
+from typing import Any
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from core.config import settings
@@ -7,7 +8,7 @@ class Base(DeclarativeBase):
     pass
 
 
-async def connect_database() -> None:
+async def connect_database() -> Any:
     engine = create_async_engine(settings.database.database_url)
     async_session = async_sessionmaker(engine)
     database = async_session()

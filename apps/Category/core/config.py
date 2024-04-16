0a01fb500d5ffa981431d474f401596b31b2f68c
whiteRR <whiterr2134@gmail.com>
2024-04-16 14:38:20 +0500

    created config file and  database settings

---
+++ apps/Category/core/config.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from os import getenv, path
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_path = path.join(BASE_DIR, ".env")
+load_dotenv(dotenv_path)
+
+
+class DataBaseSettings(BaseModel):
+    database_url: str = getenv("DATABASE_URL_CATEGORY")
+
+
+class Settings(BaseSettings):
+    database: DataBaseSettings = DataBaseSettings()
+
+
+settings = Settings()

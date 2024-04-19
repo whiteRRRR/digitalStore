562251eeeff8ef618e61877762e42b3979147696
whiteRR <whiterr2134@gmail.com>
2024-04-19 16:31:44 +0500

    added product endpoints to router

--- apps/Category/api/v1/routers.py
+++ apps/Category/api/v1/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from api.v1.endpoints.news import router as news_router
-
+from api.v1.endpoints.product import router as product_router
 
 router = APIRouter()
 router.include_router(news_router)
+router.include_router(product_router)
 

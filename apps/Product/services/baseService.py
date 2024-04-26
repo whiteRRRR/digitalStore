6b3ee317c7aaca985ac812f7da98f510e3392d4c
whiteRR <whiterr2134@gmail.com>
2024-04-26 15:19:25 +0500

    added base method for service

---
+++ apps/Product/services/baseService.py
@@ -0,0 +1,19 @@
+class BaseServices:
+    def __init__(self, repository) -> None:
+        self.repository = repository
+
+    async def add(self, scheme):
+        await self.repository.create(scheme)
+
+    async def get_all(self):
+        await self.repository.read_all()
+    
+    async def get_by_id(self, id: int):
+        await self.repository.read_by_id(id)
+    
+    async def update_by_id(self, scheme, id: int):
+        await self.repository.update_by_id(scheme, id)
+    
+    async def delete_by_id(self, id: int):
+        await self.repository.delete_by_id(id)
+    

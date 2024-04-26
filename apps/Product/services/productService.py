30f9d17dd35a93c2e1d8425ca74a4fb33f0b226a
whiteRR <whiterr2134@gmail.com>
2024-04-26 15:33:03 +0500

    created a product service and a main method for this service

---
+++ apps/Product/services/productService.py
@@ -0,0 +1,28 @@
+from services.baseService import BaseService
+from repository.productRepository import ProductRepository
+from schemes.productScheme import ProductScheme
+from core.exception import BadRequestException
+
+class ProductService(BaseService):
+    def __init__(self, repository: ProductRepository) -> None:
+        self.repository = repository
+        super().__init__(repository)
+    
+    async def create(self, scheme: ProductScheme):
+        try:
+            await self.repository.create(scheme)
+            return "Created successful"
+        except BadRequestException:
+            raise BadRequestException("Bad request")
+
+    async def get_all_product(self):
+        return self.repository.read_all()
+
+    async def delete_by_id(self, id: int):
+        try:
+            await self.delete_by_id(id)
+            return "Successful deleted"
+        except BadRequestException:
+            raise BadRequestException("Bad request")
+        
+

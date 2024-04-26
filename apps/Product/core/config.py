2c16a102d326cd8f6d84c6a810986384c73488ed
whiteRR <whiterr2134@gmail.com>
2024-04-26 12:15:47 +0500

    fixed import name

--- apps/Product/core/config.py
+++ apps/Product/core/config.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from pathlib import Path
 from os import path, getenv
-from dotenv import load_env
+from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).parent.parent
 API_V1 = "api/v1"
 ENV_PATH = path.join(BASE_DIR, ".env")
 
-load_env(ENV_PATH)
+load_dotenv(ENV_PATH)
 
 
 class DataBase(BaseModel):

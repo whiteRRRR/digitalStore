e67d9cf55f4d41c373ba93f5e1ab894ee8d4d51e
whiteRR <whiterr2134@gmail.com>
2024-04-26 15:32:08 +0500

    created bad request exception

---
+++ apps/Product/core/exception.py
@@ -0,0 +1,8 @@
+from typing import Any, Dict
+from typing_extensions import Annotated, Doc
+from fastapi import HTTPException, status
+
+
+class BadRequestException(HTTPException):
+    def __init__(self, detail: Any = None, headers: Dict[str, str] | None = None) -> None:
+        super().__init__(status.HTTP_400_BAD_REQUEST, detail, headers)

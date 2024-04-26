4e2a82666b609e186612f24d78e9c24610b3b558
whiteRR <whiterr2134@gmail.com>
2024-04-26 16:05:44 +0500

    fixed error bugs

--- apps/Product/core/config.py
+++ apps/Product/core/config.py
@@ -6,14 +6,14 @@ from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).parent.parent
-API_V1 = "api/v1"
+API_V1 = "/api/v1"
 ENV_PATH = path.join(BASE_DIR, ".env")
 
 load_dotenv(ENV_PATH)
 
 
 class DataBase(BaseModel):
-    database_url: str = getenv("DATABASE_URL_PRODUCT")
+    database_url: str = getenv("DATABASE_URL_PRODUCT") # type: ignore
 
 
 class Settings(BaseSettings):
